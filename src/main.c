5645f60791c3421cc329bccc2b4179511fbd028c
cebrianj <joserambow@gmail.com>
2024-06-18 16:01:01 +0200

    Add basic errors handling

--- src/main.c
+++ src/main.c
@@ -1,11 +1,21 @@
+#include <errno.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+#define ERROR 1
+
 struct termios initial_tsettings;
 
+void panic(const char *message) {
+    perror(message);
+    exit(ERROR);
+}
+
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &initial_tsettings);
+    if (tcgetattr(STDIN_FILENO, &initial_tsettings) == -1) {
+        panic("Error trying to retreive terminal settings");
+    }
 
     struct termios raw = initial_tsettings;
     // Modify terminal input flags
@@ -20,7 +30,9 @@ void enableRawMode() {
     // Modify terminal local flags
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        panic("Error trying to set terminal settings");
+    }
 }
 
 void set_stdin_min_read_size(int size) {
@@ -44,14 +56,16 @@ void disableRawMode() {
 int main() {
     enableRawMode();
     set_stdin_min_read_size(0);
-    float timeout_deciseconds = 1;  // 0.1 second = 100ms = 1 decisecond
+    int timeout_deciseconds = 1;  // 0.1 second = 100ms = 1 decisecond
     set_stdin_timeout(timeout_deciseconds);
     atexit(disableRawMode);
 
     char c;
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            panic("Error trying to read from terminal");
+        }
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
