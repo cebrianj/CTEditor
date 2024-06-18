79d4ff195d7eef0127d9707f8e9fa826e595a0e5
cebrianj <joserambow@gmail.com>
2024-06-18 16:27:09 +0200

    Change main to use  Utils and Terminal IO Modules

--- Makefile
+++ Makefile
@@ -1,15 +1,25 @@
 BIN_DIR=bin
+INCLUDES_DIR=include
+OBJ_DIR=obj
+SRC_DIR=src
 
 CC = gcc                    
 CFLAGS = -Wall -ggdb -pedantic -std=c99
+
+
+SRCS := $(wildcard src/*.c)
+OBJS := $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCS))
 
 TARGET: main
 all: clean TARGET
 build_run:  all run
 
 
-main:  src/main.c  $(BIN_DIR) 
-	$(CC) $(CFLAGS) $< -o $(BIN_DIR)/$@
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	$(CC) $(CFLAGS) -c $< -o $@ -I $(INCLUDES_DIR)
+
+main:  $(BIN_DIR) $(OBJ_DIR) $(OBJS)
+	$(CC) $(CFLAGS) -o $(BIN_DIR)/$@  $(OBJS)
 
 run:
 	@clear
@@ -19,6 +29,8 @@ run:
 $(BIN_DIR):
 	mkdir $(BIN_DIR)
 
+$(OBJ_DIR):
+	mkdir $(OBJ_DIR)
 
 
 ifeq ($(OS),Windows_NT)
@@ -29,5 +41,6 @@ endif
 
 clean:
 	$(RMDIR) $(BIN_DIR)
+	$(RMDIR) $(OBJ_DIR)
 
 .PHONY: clean
--- src/main.c
+++ src/main.c
@@ -1,64 +1,12 @@
 #include <errno.h>
 #include <stdlib.h>
-#include <termios.h>
 #include <unistd.h>
 
-#define ERROR 1
-
-struct termios initial_tsettings;
-
-void panic(const char *message) {
-    perror(message);
-    exit(ERROR);
-}
-
-void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &initial_tsettings) == -1) {
-        panic("Error trying to retreive terminal settings");
-    }
-
-    struct termios raw = initial_tsettings;
-    // Modify terminal input flags
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-
-    // Modify terminal output flags
-    raw.c_oflag &= ~(OPOST);
-
-    // Modify terminal control mode flags
-    raw.c_cflag |= (CS8);
-
-    // Modify terminal local flags
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
-        panic("Error trying to set terminal settings");
-    }
-}
-
-void set_stdin_min_read_size(int size) {
-    struct termios tsettings;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &tsettings);
-    tsettings.c_cc[VMIN] = 0;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &tsettings);
-}
-
-void set_stdin_timeout(int deciseconds) {
-    struct termios tsettings;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &tsettings);
-    tsettings.c_cc[VTIME] = deciseconds;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &tsettings);
-}
-
-void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &initial_tsettings);
-}
+#include "terminal_io.h"
+#include "utils.h"
 
 int main() {
-    enableRawMode();
-    set_stdin_min_read_size(0);
-    int timeout_deciseconds = 1;  // 0.1 second = 100ms = 1 decisecond
-    set_stdin_timeout(timeout_deciseconds);
-    atexit(disableRawMode);
+    setup_terminal_io();
 
     char c;
     while (1) {
@@ -67,9 +15,9 @@ int main() {
             panic("Error trying to read from terminal");
         }
         if (iscntrl(c)) {
-            printf("%d\r\n", c);
+            // printf("%d\r\n", c);
         } else {
-            printf("%d ('%c')\r\n", c, c);
+            printf("%c\n", c);
         }
         if (c == 'q') break;
     }
