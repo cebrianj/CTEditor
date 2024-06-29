3c07bcb6a4fa5b0cd8f05ec65aa477a9a90b0624
cebrianj <joserambow@gmail.com>
2024-06-29 17:49:06 +0200

    Refactor:  change main to use new user input processor and reader

--- src/main.c
+++ src/main.c
@@ -1,25 +1,17 @@
 #include <errno.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 #include "terminal_io.h"
+#include "user_input_processor.h"
+#include "user_input_reader.h"
 #include "utils.h"
 
 int main() {
-    setup_terminal_io();
-
     char c;
+    setup_terminal_io();
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-            panic("Error trying to read from terminal");
-        }
-        if (iscntrl(c)) {
-            // printf("%d\r\n", c);
-        } else {
-            printf("%c\n", c);
-        }
-        if (c == CTRL_WITH('q')) break;
+        c = read_key();
+        process_key(c);
     }
     return 0;
 }
